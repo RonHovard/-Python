dc9ad66ec09912835951a15c4722486417dd9f91
RonHovard <efremmof@yandex.ru>
2019-07-16 00:50:14 +0300

    доработал скрипт по копированию файла из которого он запущен.

--- lesson_5/copy_current_file.py
+++ lesson_5/copy_current_file.py
@@ -2,15 +2,13 @@ import os
 import shutil
 
 def copy_file(filename):
-    if os.path.isfile(filename):
-        newfile = filename + '.copy'
-        shutil.copy(filename, newfile)      #копируй
-        if os.path.exists(newfile):
-            print("Файл ", newfile, " был успешно создан")
-            return True
-        else:
-            print("Возникли проблемы копирования")
-            return False
+    # if os.path.isfile(filename):
+    newfile = filename + '.copy'
+    shutil.copy(filename, newfile)
+    if os.path.exists(newfile):
+        print("Файл ", newfile, " был успешно создан")
+    else:
+        print("Возникли проблемы копирования")
 
 
 copy_file(os.path.abspath(__file__))

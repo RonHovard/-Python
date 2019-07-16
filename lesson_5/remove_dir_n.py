0a4fb6fa53a0cd576b523c8de1e9bda74223ff30
RonHovard <efremmof@yandex.ru>
2019-07-16 13:34:43 +0300

    доделал скрипт: "удаление папок", hw05_easy.

--- lesson_5/remove_dir_n.py
+++ lesson_5/remove_dir_n.py
@@ -1,7 +1,22 @@
 import os
 
-for i in range(1, 10):
-    path_2 = 'dir_{}'.format(i)
-    if os.path.exists(path_2):
-        os.rmdir(path_2)
+# Функция удаляет нумерованные, пустые директории. Принимает две переменные:
+#  названия первой и последней дикектории (из списка директорий,
+#  которые нужно удалить) в формате: name_number
+# ,где name - имя, одинаковое для всех папок
+#  number - порядковый номер папки, после _ (нижнего подчёркивания).
+#  Например: dir_1 и dir_9
 
+
+def remove_dir(a, b):
+    name, start = a.split('_')
+    start = int(start)
+    end = int(b.split('_')[1]) + 1
+    for i in range(start, end):
+        path = '{}{}'.format(name, i)
+        if os.path.exists(path):
+            os.rmdir(path)
+
+
+if __name__ == '__main__':
+    remove_dir('dir_1', 'dir_9')

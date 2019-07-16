cefb7b8843ef2d5694fb55a87cfdf67bfe8d321e
RonHovard <efremmof@yandex.ru>
2019-07-16 15:36:44 +0300

    Доработал hw05_easy.py

--- lesson_5/hw05_easy.py
+++ lesson_5/hw05_easy.py
@@ -7,21 +7,27 @@ import shutil
 # И второй скрипт, удаляющий эти папки.
 
 #  Функция создаёт нумерованные, пустые директории. Принимает две переменные:
-#   названия первой и последней дикектории (из списка директорий, который
-#   нужно создать) в формате: name_number
-#   , где name - имя, одинаковое для всех папок
+#  названия первой и последней дикектории (из списка директорий, который
+#  нужно создать) в формате: name_number
+#  , где name - имя, одинаковое для всех папок
 #  number - порядковый номер папки, после _ (нижнего подчёркивания).
 #  Например: creation_dir('dir_1', 'dir_9')
+#  Для создания одной директории достаточно указать её имя в
+#  поле для аргументов два раза через ",".
+#  Например: creation_dir('dir', 'dir')
 
 
 def creation_dir(a, b):
-    name, start = a.split('_')
-    start = int(start)
-    end = int(b.split('_')[1]) + 1
-    for i in range(start, end):
-        path = '{}{}'.format(name, i)
-        if not os.path.exists(path):
-            os.mkdir(path)
+    try:
+        name, start = a.split('_')
+        start = int(start)
+        end = int(b.split('_')[1]) + 1
+        for i in range(start, end):
+            path = '{}{}'.format(name, i)
+            if not os.path.exists(path):
+                os.mkdir(path)
+    except ValueError:
+        os.mkdir(a)
 
 
 if __name__ == '__main__':
@@ -36,13 +42,14 @@ if __name__ == '__main__':
 
 
 def remove_dir(a, b):
-    name, start = a.split('_')
-    start = int(start)
-    end = int(b.split('_')[1]) + 1
-    for i in range(start, end):
-        path = '{}{}'.format(name, i)
-        if os.path.exists(path):
-            os.rmdir(path)
+        name, start = a.split('_')
+        start = int(start)
+        end = int(b.split('_')[1]) + 1
+        for i in range(start, end):
+            path = '{}{}'.format(name, i)
+            if os.path.exists(path):
+                os.rmdir(path)
+
 
 
 if __name__ == '__main__':

9abe28798e374e5f75fd33a1271a1929ddee22e9
RonHovard <efremmof@yandex.ru>
2019-07-16 13:34:18 +0300

    доделал скрипт: "создание папок", hw05_easy.

--- lesson_5/creat_dir_n.py
+++ lesson_5/creat_dir_n.py
@@ -1,10 +1,26 @@
 
 import os
 
-for i in range(1, 10):
-    path = 'dir_{}'.format(i)
-    if not os.path.exists(path):
-        os.mkdir(path)
+#  Функция создаёт нумерованные, пустые директории. Принимает две переменные:
+#  названия первой и последней дикектории (из списка директорий, который
+#  нужно создать) в формате: 'name_number'
+#  , где name - имя, одинаковое для всех папок
+#  number - порядковый номер папки, после _ (нижнего подчёркивания).
+#  Например: creation_dir('dir_1', 'dir_9')
+
+
+def creation_dir(a, b):
+    name, start = a.split('_')
+    start = int(start)
+    end = int(b.split('_')[1]) + 1
+    for i in range(start, end):
+        path = '{}{}'.format(name, i)
+        if not os.path.exists(path):
+            os.mkdir(path)
+
+
+if __name__ == '__main__':
+    creation_dir('dir_1', 'dir_9')
 
 
 

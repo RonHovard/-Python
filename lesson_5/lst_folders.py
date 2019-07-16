df43ae376512d78c176a3954f46f898ec58624af
RonHovard <efremmof@yandex.ru>
2019-07-16 13:35:42 +0300

    доделал скрипт: "отображающий папки тек. дир.", hw05_easy

--- lesson_5/lst_folders.py
+++ lesson_5/lst_folders.py
@@ -1,8 +1,15 @@
 import os
 
-lst_f = list()
-for i in os.listdir():
-    if not os.path.isfile(i):
-        lst_f.append(i)
+# Функция возвращает список папок в текущей директории
 
-print(lst_f)
+
+def lst_folders():
+    lst_f = list()
+    for i in os.listdir():
+        if not os.path.isfile(i):
+            lst_f.append(i)
+    return lst_f
+
+
+if __name__ == '__main__':
+    print(lst_folders())
